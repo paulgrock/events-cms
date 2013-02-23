0d2f86033672850b6b50411e4a662fc736e2a86e
cybergamer <james@cybergamer.com.au>
2013-02-24 01:54:25 +1030

    * Restructured the way Teams are handled on a matchup. * Resolved a bug where changing a team after creation would result in the new team not appearing as an option in the game "winner" dropdown. * Added team thumbnails to event and matchup creation/edit * Added holder.js to vendors for handling of generic thumbs.

--- views/src/javascripts/models/event.js
+++ views/src/javascripts/models/event.js
@@ -14,13 +14,18 @@ Koala.models.add('event', Backbone.Model.extend({
 	initialize: function() {
 		this.matchup = Koala.models.new('matchup', this.get('matchup'));
 
+		//Set matchup values when event values change from external source
 		this.on("change:matchup", function() {
 			this.matchup.set(this.get('matchup'));
 		});
 
+		//Set values on matchup change
 		this.listenTo(this.matchup, "change", function() {
 			this.set({matchup: this.matchup.attributes}, {silent: true});
 		});
+
+		//Set default values
+		this.set({matchup: this.matchup.attributes}, {silent:true});
 	}
 
 }));
--- views/src/javascripts/views/game_action_td.js
+++ views/src/javascripts/views/game_action_td.js
@@ -4,11 +4,11 @@ Koala.views.add('game_action_td', Backbone.View.extend({
 
 	events: {
 		"click .teamA_select" : function() {
-			this.model.set('winner', this.matchup.teams[0]);
+			this.model.set('winner', this.matchup.get("teams")[0]);
 			this.end();
 		},
 		"click .teamB_select" : function() {
-			this.model.set("winner", this.matchup.teams[1]);
+			this.model.set("winner", this.matchup.get("teams")[1]);
 			this.end();
 		},
 		"click .start_select" : "start"
@@ -16,12 +16,13 @@ Koala.views.add('game_action_td', Backbone.View.extend({
 
 	initialize: function(options) {
 		this.matchup = options.matchup;
+		this.listenTo(this.matchup, "change:teams", this.render);
 	},
 
 	render: function() {
 		var data = $.extend(true, {}, this.model.attributes);
-		data.teamA = this.matchup.teams && this.matchup.teams[0];
-		data.teamB = this.matchup.teams && this.matchup.teams[1];
+		data.teamA = this.matchup.get("teams") && this.matchup.get("teams")[0];
+		data.teamB = this.matchup.get("teams") && this.matchup.get("teams")[1];
 
 		this.$el.html(this.getTemplate()(data))
 
@@ -31,7 +32,7 @@ Koala.views.add('game_action_td', Backbone.View.extend({
 	},
 
 	getTemplate: function() {
-		switch(this.model.attributes.status) {
+		switch(this.model.get('status')) {
 			case "underway":
 				return Koala.templates.get('game_action_td_underway');
 
