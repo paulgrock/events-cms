925ca8012519fc744e128ca599a79276a33f51f8
Paul Grock <paulgrock@gmail.com>
2013-04-18 14:19:03 -0700

    cors for server sent events.

--- routes/server_side_events.js
+++ routes/server_side_events.js
@@ -1,46 +1,42 @@
 var connections = [];
 
 var removeConnection = function(res){
-  var index = connections.indexOf(res);
-  connections.splice(index, 1);
-}
+    'use strict';
+    var index = connections.indexOf(res);
+    connections.splice(index, 1);
+};
 
 // Global function to trigger server side event
 app.notify = function(method, type, data){
-  var message = '';
-  data = JSON.parse(data);
-  var messageObj = {
-    method: method,
-    title: data.title,
-    matchup: data.matchup,
-    starts_at: data.starts_at
-  }
-  message += 'event: ' + type + '\n';
-  message += 'data: ' + JSON.stringify(messageObj) + '\n\n';
-  console.log(message);
-  connections.forEach(function(res){
-    res.write(message);
-  })
-}
+    'use strict';
+    var message = '';
+    data = JSON.parse(data);
+    var messageObj = {
+        method: method,
+        title: data.title,
+        matchup: data.matchup,
+        starts_at: data.starts_at
+    };
+    message += 'event: ' + type + '\n';
+    message += 'data: ' + JSON.stringify(messageObj) + '\n\n';
+    connections.forEach(function(res){
+        res.write(message);
+    });
+};
 
 exports.index = function(req, res){
-  if(req.accepts('text/event-stream')){
+    'use strict';
     res.header({
-      'content-type': 'text/event-stream',
-      'cache-control': 'no-cache',
-      'connection': 'keep-alive',
-      'Access-Control-Allow-Origin': "*",
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type'
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Origin': req.headers.origin
     });
     if(req.xhr) {
-      res.xhr = null;
+        res.xhr = null;
     }
 
     connections.push(res);
     res.write('id\n');
     req.on('close', removeConnection);
-  } else {
-    res.send(200, 'foo');
-  }
 };
