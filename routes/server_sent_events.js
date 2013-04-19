d838a34b543bde3fe6844aec25b9539483d5d4d6
Paul Grock <paulgrock@gmail.com>
2013-04-19 12:15:13 -0700

    a handful of fixes to hopefully solve ec2 CORS issues

--- routes/server_sent_events.js
+++ routes/server_sent_events.js
@@ -1,9 +1,9 @@
 var connections = [];
-
-var removeConnection = function(res){
+var removeConnection = function(res, interval){
     'use strict';
     var index = connections.indexOf(res);
     connections.splice(index, 1);
+    clearInterval(interval);
 };
 
 // Global function to trigger server side event
@@ -26,28 +26,41 @@ app.notify = function(method, type, data){
 
 exports.index = function(req, res){
     'use strict';
-    if (req.method === "OPTIONS") {
+    if (req.method === 'OPTIONS') {
         res.writeHead(200, {
-            "Access-Control-Allow-Origin": req.headers.origin,
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Last-Event-ID, Cache-Control",
-            "Access-Control-Max-Age": "86400"
+            'Access-Control-Allow-Origin': req.headers.origin,
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Last-Event-ID, Cache-Control',
+            'Access-Control-Max-Age': '86400'
         });
         res.end();
         return;
     }
+    if(req.accepts('text/event-stream')){
+        res.header({
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Origin': req.headers.origin,
+            'Access-Control-Allow-Headers': 'Last-Event-ID, Cache-Control',
+            'Access-Control-Max-Age': '86400'
+        });
+        if(req.xhr) {
+            res.xhr = null;
+        }
 
-    res.header({
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': req.headers.origin
-    });
-    if(req.xhr) {
-        res.xhr = null;
+        connections.push(res);
+        res.write('id\n');
+
+        var interval = setInterval(function () {
+            res.write('data: ' + Date() + '\n\n');
+        }, 1000);
+
+        req.on('close', function(){
+            removeConnection(res, interval);
+        });
+    } else {
+        res.send('');
     }
-
-    connections.push(res);
-    res.write('id\n');
-    req.on('close', removeConnection);
 };
